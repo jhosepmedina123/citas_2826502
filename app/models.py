7186233ce266d4d2607ae7df6754dbc7b9b0796d
DarwinL14 <dslesmes6@soy.sena.edu.co>
2024-02-28 17:14:15 -0500

    ultima actualizacion

--- app/models.py
+++ app/models.py
@@ -14,6 +14,8 @@ class Medico (db.Model):
     numero_identificacion = db.Column(db.Integer)
     registro_medico = db.Column(db.Integer)
     especialidad = db.Column(db.String(50))
+
+    citas = db.relationship("Cita" , backref = "medico")
     
 class Paciente(db.Model):
     
